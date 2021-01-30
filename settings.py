8917f7f92b50f56a5b4cb9a97977425d61df0de9
Devan Mallory <32146940+dman82499@users.noreply.github.com>
2021-01-30 01:33:38 -0500

    updated to be more personalized

--- settings.py
+++ settings.py
@@ -1,14 +1,13 @@
 import os
 
-# The prefix that will be used to parse commands.
-# It doesn't have to be a single character!
-COMMAND_PREFIX = "!"
+
+COMMAND_PREFIX = "->"
 
 # The bot token. Keep this secret!
 BOT_TOKEN = "Get this from https://discord.com/developers/applications"
 
 # The now playing game. Set this to anything false-y ("", None) to disable it
-NOW_PLAYING = COMMAND_PREFIX + "commands"
+NOW_PLAYING = COMMAND_PREFIX + "lmaooooooooooooo"
 
 # Base directory. Feel free to use it if you want.
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
